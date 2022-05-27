eef81eee83bb9bb28e494e6d0fdcb629476424a3
Emily Reese <emilyreese.ga@gmail.com>
2022-05-27 17:03:08 -0400

    Album search project

--- index.js
+++ index.js
@@ -1,26 +1,62 @@
 const url = "https://itunes.apple.com/search?term=${ARTIST_NAME}&media=music&entity=album&attribute=artistTerm&limit=200"
-var albumName;
-var albumCover;
-var artistName; 
-var albumCount;
-var albumInfo;
+const searchInput = document.getElementsByClassName('searchInput');
+const searchButton = document.getElementsByClassName('searchButton');
 
 
-  
+const search = () => {
+    searchButton.addEventListener('click', function(e){
+    let artist = searchInput.value
+    if (!artist){
+        alert('Please enter an artist name')
+        return
+    }
+
+    document.querySelector(".loader").style.display = "block"
+
     fetchJsonp(url)
     .then(res => res.json())
-    .then(json => console.log(json));
-
-
-
-function createAlbumTmp (artistName) {
-return `<section style="display: flex; justify-content: space-between;">
-  <img src="https://placeimg.com/320/240/tech" alt="Preview of Whizzbang Widget">
-  <h3>Whizzbang Widget <em>$25</em></h3>
-  <p>Liquorice candy macaroon soufflé jelly cake. Candy canes ice cream biscuit marzipan. Macaroon pie sesame
-    snaps
-    jelly-o.
-  </p>
-  <a href="#">Add to Cart</a>
-</section>`
+    .then((res) => {
+        console.log(res.results)
+        })
+    })
 }
+
+searchInput.addEventListener('keyup', function (event) {
+    if (event.keyCode === 13) {
+      searchButton.click()
+    }
+  })
+
+
+      function showAlbums(data){
+        document.querySelector(".loader").style.display = "none"
+        let numOfAlbums = data.length
+    
+ 
+      const albumCount = document.createElement('h1')
+      albumCount.classList.add("result-h1")
+      albumCount.innerHTML = `${numOfAlbums} albums found for "${searchInput.value}"`
+      document.querySelector('albumCount').appendChild(albumCount)
+
+      const albumContainer = document.createElement('section')
+     albumContainer.classList.add("container")
+      document.querySelector('.results').appendChild(albumContainer)
+
+      data.forEach(album => {
+        const albumNames = document.createElement('h4')
+        albumNames.classList.add('albumName')
+
+        albumNames.innerText = album.collectionName
+        albumDiv.appendChild(albumNames)
+
+        const albumInfo = document.createElement('ul')
+        albumInfo.classList.add('albumInfo')
+
+        const albumCovers = document.createElement('img')
+        albumCovers.classList.add('albumCover')
+
+        albumCovers.src = album.artworkUrl100
+        albumDiv.appendChild(albumCovers)
+        document.querySelector('container').appendChild(albumInfo)
+      })
+      }
